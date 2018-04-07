309e248c6e58bdff0b0f44848fc5dc62d5155197
Calle Englund <calle@discord.bofh.se>
2018-04-07 18:36:57 +0200

    Refactor loader initialization
    Trying to define a new loader before a fetch had been attempted crashed, because `@loaders` wasn’t initialized.

--- lib/key_tree/loader.rb
+++ lib/key_tree/loader.rb
@@ -6,16 +6,23 @@ module KeyTree
       yaml: 'YAML', yml: 'YAML'
     }.freeze
 
-    def self.[](loader_type)
+    def self.[](type)
+      type = type.to_sym if type.respond_to?(:to_sym)
+      loaders[type]
+    end
+
+    def self.[]=(type, loader_class)
+      type = type.to_sym if type.respond_to?(:to_sym)
+      loaders[type] = loader_class
+    end
+
+    private_class_method
+
+    def self.loaders
       @loaders ||= BUILTIN_LOADERS.each_with_object({}) do |pair, result|
         type, name = pair
         result[type] = const_get(name) if const_defined?(name)
       end
-      @loaders[loader_type]
-    end
-
-    def self.[]=(type, loader_class)
-      @loaders[type] = loader_class
     end
   end
 end
