58ec4da2faecd155de751088211440e5f37fc54f
Calle Englund <calle@discord.bofh.se>
2018-09-05 21:32:56 +0200

    Fix Lint/UselessAccessModifier offenses
    `private_class_method` is considered useless in a Module, so wrap all the `def self.x` things in `class << self` and use regular `private` declaration instead.

--- lib/key_tree/loader.rb
+++ lib/key_tree/loader.rb
@@ -10,26 +10,27 @@ module KeyTree
       yaml: 'YAML', yml: 'YAML'
     }.freeze
 
-    def self.[](type)
-      type = type.to_sym if type.respond_to?(:to_sym)
-      loaders[type] || @fallback
-    end
+    class << self
+      def [](type)
+        type = type.to_sym if type.respond_to?(:to_sym)
+        loaders[type] || @fallback
+      end
 
-    def self.[]=(type, loader_class)
-      type = type.to_sym if type.respond_to?(:to_sym)
-      loaders[type] = loader_class
-    end
+      def []=(type, loader_class)
+        type = type.to_sym if type.respond_to?(:to_sym)
+        loaders[type] = loader_class
+      end
 
-    def self.fallback(loader)
-      @fallback = loader
-    end
+      attr_writer :fallback, :loaders
+      alias fallback fallback=
 
-    private_class_method
+      private
 
-    def self.loaders
-      @loaders ||= BUILTIN_LOADERS.each_with_object({}) do |pair, result|
-        type, name = pair
-        result[type] = const_get(name) if const_defined?(name)
+      def loaders
+        @loaders ||= BUILTIN_LOADERS.each_with_object({}) do |pair, result|
+          type, name = pair
+          result[type] = const_get(name) if const_defined?(name)
+        end
       end
     end
   end
